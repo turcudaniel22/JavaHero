38cfc86e2385f0f74d695530310f7b7c10e5c3f2
Danielsuna <turcudanieli22@gmail.com>
2022-12-19 12:11:09 +0100

    rolled back project. working

---
+++ src/RPG/Interface/HeroesInterface.java
@@ -0,0 +1,15 @@
+//package RPG.Interface;
+//
+//import RPG.Character.Character.Mage;
+//import RPG.Character.Character.Warrior;
+//import RPG.Character.Character.Rogue;
+//import RPG.Character.Character.Ranger;
+//
+//public class HeroesInterface {
+//    Mage mage = new Mage("mage");
+//    Warrior warrior = new Warrior("dfd");
+//    Rogue rogue = new Rogue("sadsa");
+//    Ranger ranger = new Ranger("dff");
+//
+//
+//}
